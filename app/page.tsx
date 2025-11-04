aed5a3becae66fd31e848f60e39a101b8b100e53
moelsayed011 <me6758478@gmail.com>
2025-11-04 10:46:11 +0200

    fixing error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import CarList from "@/components/car/CarList";
 import HeaderSection from "@/components/layout/HeaderSection";
 import PickAndDropWrapper from "@/components/pick-up&drop-off/PickAndDropWrapper";
+import AddReviewForm from "@/components/review/AddReviewForm";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function Home({
   searchParams,
@@ -10,11 +12,13 @@ export default async function Home({
   const params = await searchParams;
   const page = Number(params?.page) || 1;
 
+  const { userId } = await auth();
   return (
-    <div className="container">
+    <div className="container mb-5">
       <HeaderSection />
       <PickAndDropWrapper />
-      <CarList page={page} />
+      <AddReviewForm userId={userId} />
+      {/* <CarList page={page} /> */}
     </div>
   );
 }
