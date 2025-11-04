10faa915fab1aab474455c3bd4d597858599bd30
moelsayed011 <me6758478@gmail.com>
2025-11-04 11:39:20 +0200

    move car list

--- app/car/page.tsx
+++ app/car/page.tsx
@@ -1,7 +1,12 @@
+import CarList from "@/components/car/CarList";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="mt-24 container">
+      <CarList />
+    </div>
+  );
 };
 
 export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,15 @@
-import CarList from "@/components/car/CarList";
 import HeaderSection from "@/components/layout/HeaderSection";
 import PickAndDropWrapper from "@/components/pick-up&drop-off/PickAndDropWrapper";
 import AddReviewForm from "@/components/review/AddReviewForm";
 import { auth } from "@clerk/nextjs/server";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ page?: string }>;
-}) {
-  const params = await searchParams;
-  const page = Number(params?.page) || 1;
-
+export default async function Home() {
   const { userId } = await auth();
   return (
     <div className="container mb-5">
       <HeaderSection />
       <PickAndDropWrapper />
       <AddReviewForm userId={userId} />
-      <CarList />
     </div>
   );
 }
